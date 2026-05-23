45282e5942c69bbf2de49eccc627f26d3a1eb09e
havoc398 <srssahu9721@gmail.com>
2026-05-23 08:59:38 +0530

    `Added login form to Login page`

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -2,7 +2,20 @@ import React from "react"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <main>
+    <div className="form-container">
+      <h1>Login</h1>
+
+      <form>
+
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input type="text" id = "email" name = "email" placeholder="Enter email address"/>
+        </div>
+
+      </form>
+    </div>
+    </main>
   )
 }
 

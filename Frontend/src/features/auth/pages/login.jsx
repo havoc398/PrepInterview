b74f644591345ba557e342f21421ebd8c01dfe84
havoc398 <srssahu9721@gmail.com>
2026-05-25 14:11:46 +0530

    Implement authentication context and API integration with login and registration functionality

--- Frontend/src/features/auth/pages/login.jsx
+++ Frontend/src/features/auth/pages/login.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate, Link } from 'react-router'
 import "../auth.form.scss"
+import { useAuth } from '../hooks/useAuth'
 const Login = () => {
 
-  
-  const handleSubmit =(e) =>{
+  const { loading, handleLogin } = useAuth()
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = (e) => {
     e.preventDefault()
   }
 
@@ -12,21 +18,25 @@ const Login = () => {
     <main>
       <div className="form-container">
         <h1>Login</h1>
-
         <form onSubmit={handleSubmit}>
-
           <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input type="email" id='email' name='email' placeholder='Enter email address' />
+            <input
+              onChange={(e) => { setEmail(e.target.value) }}
+              type="email" id='email' name='email' placeholder='Enter email address' />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" id='password' name='password' placeholder='Enter the password' />
+            <input
+            onChange={(e)}
+            type="password" id='password' name='password' placeholder='Enter the password' />
           </div>
 
           <button className='button primary-button'>Login</button>
 
         </form>
+
+        <p>Don't have an account? <Link to={"/register"}>Register</Link> </p>
       </div>
     </main>
   )
